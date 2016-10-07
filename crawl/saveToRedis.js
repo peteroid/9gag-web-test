ceec91171da1e1c7c3873ec2a5fe893eb435d427
PETEroid <selllfocard@gmail.com>
2016-10-07 14:03:42 +0800

    update crawling

--- crawl/saveToRedis.js
+++ crawl/saveToRedis.js
@@ -6,14 +6,6 @@ const redisServerConf = {
   port: '6379'
 }
 
-var redisClient = redis.createClient(redisServerConf.port, redisServerConf.host)
-
-redisClient.on('connect', function () {
-  console.log('Redis connected')
-  var posts = getPostsFromFile('/posts.txt')
-  savePostsToRedis(posts)
-})
-
 var getPostsFromFile = function (fileName) {
   return JSON.parse(fs.readFileSync(__dirname + fileName, 'utf8'))
 }
@@ -21,27 +13,70 @@ var getPostsFromFile = function (fileName) {
 var savePostsToRedis = function (posts) {
   var postsString = posts.map(_ => {return JSON.stringify(_)})
   console.log(postsString.length)
+
+  var postCodeList = posts.map(_ => {return _.code})
+  console.log(postCodeList.slice(0, 5))
+
+  var postHashes = {}
+  var postHashList = posts.map(p => {
+    let h = {
+      comments_count: p.comments.count,
+      likes_count: p.likes.count,
+      date: p.date,
+      raw: JSON.stringify(p)
+    }
+
+    postHashes[p.code] = h
+    return h
+  })
+
+  // console.log(postHashList.slice(0, 5))
+  // for (var k in postHashes) {
+  //   let v = postHashes[k]
+  //   console.log(k, v)
+  // }
   // return
-  redisClient.exists('posts', function (err, exists) {
+
+  redisClient.rpush(['post_code_list'].concat(postCodeList), (err, reply) => {
     if (err) {
       console.log(err)
-    } else if (exists) {
-      console.log('posts exist. del the old posts')
-      redisClient.del('posts', (err, reply) => {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log('save now')
-          redisClient.rpush(['posts'].concat(postsString))
-        }
-      })
     } else {
-      console.log('posts not exist. save now')
-      redisClient.rpush(['posts'].concat(postsString))
+      console.log('post code list saved')
+      for (var k in postHashes) {
+        let v = postHashes[k]
+        console.log('saving: %s -> %s', k, v)
+        redisClient.hmset('h_' + k, v)
+      }
     }
   })
+
+  // redisClient.exists('posts', function (err, exists) {
+  //   if (err) {
+  //     console.log(err)
+  //   } else if (exists) {
+  //     console.log('posts exist. del the old posts')
+  //     redisClient.del('posts', (err, reply) => {
+  //       if (err) {
+  //         console.log(err)
+  //       } else {
+  //         console.log('save now')
+  //         redisClient.rpush(['posts'].concat(postsString))
+  //       }
+  //     })
+  //   } else {
+  //     console.log('posts not exist. save now')
+  //     redisClient.rpush(['posts'].concat(postsString))
+  //   }
+  // })
 }
 
+var posts = getPostsFromFile('/posts.txt')
+var redisClient = redis.createClient(redisServerConf.port, redisServerConf.host)
+redisClient.on('connect', function () {
+  console.log('Redis connected')
+  savePostsToRedis(posts)
+})
+
 module.exports = {
-  posts: getPostsFromFile('/posts.txt')
+  posts: posts
 }
